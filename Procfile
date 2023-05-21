2a87ec580e76f1c3f4a7b215c168a2f401867d45
jenil111 <jenilshyara@gmail.com>
2023-05-21 17:17:40 +0530

    some changes

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn pet_project.wsgi
--- pet_project/settings.py
+++ pet_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -148,5 +147,3 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-django_heroku.settings(locals())
